abf42dcfe296b65e12b97a5bd7d9f0295418f76b
Jiler01 <jl10141516@gmail.com>
2023-04-28 21:44:36 +0200

    collide again

--- Python learning journey/Nuit du code/Training/collide.py
+++ Python learning journey/Nuit du code/Training/collide.py
@@ -1,20 +1,13 @@
-def collide(obj1,obj2):
-    x1_1 = obj1.x
-    y1_1 = obj1.y
+def collide(rect1, rect2):
+    # Coordonnées des coins supérieur gauche et inférieur droit des rectangles
+    rect1_coords = (rect1.x, rect1.y, rect1.x + rect1.w, rect1.y + rect1.h)
+    rect2_coords = (rect2.x, rect2.y, rect2.x + rect2.w, rect2.y + rect2.h)
 
-    x2_1 = obj1.x + obj1.w
-    y2_1 = obj1.y + obj1.h
+    # Vérification de la collision selon l'axe x
+    x_overlap = not (rect1_coords[2] < rect2_coords[0] or rect2_coords[2] < rect1_coords[0])
 
+    # Vérification de la collision selon l'axe y
+    y_overlap = not (rect1_coords[3] < rect2_coords[1] or rect2_coords[3] < rect1_coords[1])
 
-    x1_2 = obj2.x
-    y1_2 = obj2.y
-
-    x2_2 = obj2.x + obj2.w
-    y2_2 = obj2.y + obj2.h
-
-    Collision.left = x1_1 < x2_2
-    Collision.right = x1_2 < x2_1
-    Collision.top = y1_1 < y2_2
-    Collision.bottom = y1_2 < y2_1
-    Collision.happens = Collision.left or Collision.right or Collision.top or Collision.bottom
-    return Collision
+    # Si les deux conditions sont vraies, les rectangles se chevauchent
+    return x_overlap and y_overlap
