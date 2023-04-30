aa9817d3fcb1afb02ce18950e1d7353cb154eb7d
Jiler01 <jl10141516@gmail.com>
2023-04-30 14:13:13 +0200

    checkpoint

--- Python learning journey/Nuit du code/Tycoon/main.py
+++ Python learning journey/Nuit du code/Tycoon/main.py
@@ -3,14 +3,13 @@ import pyxel as px
 class App:
     def __init__(self,fps=60,window_size:dict={"x":120 ,"y":120}):
         px.init(window_size["x"],window_size["y"],fps=fps,title="Little Tycoon")
-        px.load("PYXEL_RESOURCE_FILE.pyxres")
+        px.load("PYXEL_RESSOURCE_FILE.pyxres")
         px.run(self.update, self.draw)
 
     def update(self):
         pass
 
     def draw(self):
-        px.cls(px.COLOR_BLACK)
-        px.bltm(0,0,0,0,0,8,8,px.COLOR_BLACK)
+        px.bltm(0,0,0,0,0,256,256,px.COLOR_BLACK)
 
 App()
